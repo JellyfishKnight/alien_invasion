91a851f68d3bf86d665bb1b756aba0b750576c1f
JellyfishKngiht <1002984726@qq.com>
2022-09-17 21:07:34 +0800

    加入外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 import pygame
+from pygame.sprite import Group
 import game_functions as gf
 from settings import Settings
 from ship import Ship
-from pygame.sprite import Group
 
 
 def run_game():
@@ -14,17 +14,20 @@ def run_game():
     ))
     pygame.display.set_caption("Alien Invasion")
 
-    # 创建一艘飞船
+    # 创建一艘飞船,一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
     # 创建一个用于储存子弹的编组
     bullets = Group()
+    aliens = Group()
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
