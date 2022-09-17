4f2a02729d06b0d806e22d0f9453c468519e089c
JellyfishKngiht <1002984726@qq.com>
2022-09-17 16:21:57 +0800

    init commit

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import pygame
+import game_functions as gf
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    # 初始化游戏并且创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((
+        ai_settings.screen_width, ai_settings.screen_height
+    ))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
+
+    # 开始游戏的主循环
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
