000878d4dd209e169fe06916f462fad546d78b0c
JellyfishKngiht <1002984726@qq.com>
2022-09-18 15:11:50 +0800

    使外星人群向下移动并且能够与子弹碰撞

--- game_functions.py
+++ game_functions.py
@@ -60,10 +60,18 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
     pygame.display.flip()
 
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     # 更新子弹的位置并且删除已经消失的子弹
     # 更新子弹的位置
     bullets.update()
+    # 检查是否有子弹击中了外星人
+    # 如果是,就删除对应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if len(aliens) == 0:
+        # 删除现有的子弹并且新建一群外星人
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
 
     # 删除已经消失的子弹
     for bullet in bullets.copy():
@@ -107,3 +115,25 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number, row_number)
+
+
+def check_fleet_edges(ai_settings, aliens):
+    # 有外星人到达边缘时采取相应的措施
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+
+def change_fleet_direction(ai_settings, aliens):
+    # 将整群外星人下移,并改变它们的方向
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
+
+
+def update_aliens(ai_settings, aliens):
+    # 检查是否有外星人位于屏幕边缘
+    check_fleet_edges(ai_settings, aliens)
+    # 更新外星人群中所有外星人的位置
+    aliens.update()
--- settings.py
+++ settings.py
@@ -8,10 +8,17 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # 飞船的设置
-        self.ship_speed_factor = 0.7
+        self.ship_speed_factor = 0.6
         # 子弹的设置
         self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 10
+        self.bullet_allowed = 3
+        # 外星人设置
+        self.alien_speed_factor = 0.2
+        self.fleet_drop_speed = 5
+        # fleet_direction为1表示向右移动, -1表示想左移动
+        self.fleet_direction = 1
+
+
