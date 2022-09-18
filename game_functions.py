6abfaee0926798304e07c895fa4a9ca38c56f9f0
JellyfishKngiht <1002984726@qq.com>
2022-09-18 15:16:40 +0800

    重构碰撞检测

--- game_functions.py
+++ game_functions.py
@@ -65,18 +65,22 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
     # 更新子弹的位置
     bullets.update()
     # 检查是否有子弹击中了外星人
-    # 如果是,就删除对应的子弹和外星人
+    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    # 删除已经消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+    # 响应子弹和外星人的碰撞
+    # 删除发生碰撞的子弹和外星人
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if len(aliens) == 0:
         # 删除现有的子弹并且新建一群外星人
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
-
-    # 删除已经消失的子弹
-    for bullet in bullets.copy():
-        if bullet.rect.bottom <= 0:
-            bullets.remove(bullet)
 
 
 def get_number_aliens_x(ai_settings, alien_width):
