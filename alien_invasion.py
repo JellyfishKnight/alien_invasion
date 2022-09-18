839dd266cb81ccc20dd01d69832b86acf07e3f2a
JellyfishKngiht <1002984726@qq.com>
2022-09-18 16:47:26 +0800

    添加记分板

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 from game_stats import GameStats
 from button import Button
+from scoreboard import Scoreboard
 
 
 def run_game():
@@ -18,8 +19,9 @@ def run_game():
 
     # 创建play按钮
     play_button = Button(ai_settings, screen, "Play")
-    # 创建一个用于储存游戏统计信息的实例
+    # 创建一个用于储存游戏统计信息的实例和记分板
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 创建一艘飞船,一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
@@ -36,7 +38,7 @@ def run_game():
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -73,7 +73,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
 
-def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
+def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     # 更新屏幕上的图像并且切换到新屏幕
     # 每次循环的时候重新绘制屏幕
     screen.fill(ai_settings.bg_color)
@@ -82,6 +82,8 @@ def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button
         bullet.draw_bullet()
     ship.blitme()
     aliens.draw(screen)
+
+    sb.show_score()
 
     # 如果游戏处于非活动状态,就绘制Play按钮
     if not stats.game_active:
